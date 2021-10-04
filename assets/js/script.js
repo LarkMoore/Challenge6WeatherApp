d43a3032a555ce219262dd56df9c410545996fe2
LarkMoore <moore.lark@gmail.com>
2021-10-04 02:56:41 -0400

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,55 @@ let longitude = 0.0
 
 
 input= addEventListener ("keyup" , function(event) {
+
     if (keyCode ==13 ){
         event.preventDefault();
 
         city=input.value
+        getSearchWeather(city)
+        console.log(city)
     }
-    
 })
+const weather ={}
+
+weather.temperature ={
+    unit: "celsius"
+}
+
+const KELVIN =273
+
+const key= '8ea2e6b67fa9258a21713b8c7a16a2f7'
+
+if("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition,showError)
+} else {
+    notificationElement.style.display ='block'
+    notificationElement.innerHTML='<p> Browser does not support geolocation </p>'
+}
+
+function setPosition(position) {
+    latitude =position.coords.latitude
+    longitude =position.coords.longitude
+
+    getWeather(latitude,longitude)
+}
+locationIcon.addEventListener("click" ,function(event){
+    console.log('helloworld')
+    getWeather(latitude,longitude)
+})
+
+function showError(error) {
+    notificationElement.style.display= "block"
+    notificationElement.innerHTML= '<p> ${error.message} </p>'
+}
+function getSearchWeather(city) {
+    let api=
+
+    fetch(api)
+    .then{function (response) {
+        let data= response.json ()
+        return data
+    })
+}
+
+
