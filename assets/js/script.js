1ce9695e1a5ced13a4a2f23bd88774a637c6a212
LarkMoore <moore.lark@gmail.com>
2021-10-04 13:08:05 -0400

    weather functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,13 +54,48 @@ function showError(error) {
     notificationElement.innerHTML= '<p> ${error.message} </p>'
 }
 function getSearchWeather(city) {
-    let api=
+    let api= 'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key]'
 
     fetch(api)
-    .then{function (response) {
+    .then(function (response) {
         let data= response.json ()
         return data
     })
+    .then(function( data) {
+        weather.temperature.value=Math.floor (data.main.temp -KELVIN)
+        weather.description=data.weather[0].description
+        weather.iconId= data.weather[0].icon
+        weather.city=data.name
+        weather.country= data.sys.country
+    })
+    .then(function() {
+        displayWeather ()
+    })
+}
+function getWeather(latitude,longitude) {
+    let api= 'http://api.openweathermap.org/data/2.5/weather?lat=${latitude}lon=${longitude}&appid=${key}'
+
+    fetch(api)
+    .then(function (response) {
+        let data= response.json ()
+        return data
+    })
+    .then(function( data) {
+        weather.temperature.value=Math.floor (data.main.temp -KELVIN)
+        weather.description=data.weather[0].description
+        weather.iconId= data.weather[0].icon
+        weather.city=data.name
+        weather.country= data.sys.country
+    })
+    .then(function() {
+        displayWeather ()
+    })
 }
 
-
+function displayWeather () {
+    
+    iconElement.innerHTML=<img src=icons/${weather.iconId}.png />
+    tempElement.innerHTML=${weather.temperature.value} *<span>C<span>
+    descElement.innerHTML=weather.description
+    locationElement.innerHTML=${weather.city}, ${weather.country}
+}
